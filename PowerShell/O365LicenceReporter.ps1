c63ae98b10d74bbafac81899469d4704a2409270
Kacper Michta <kacper.michta@itcentric.co.uk>
2020-03-06 10:52:15 +0000

    Will Output to powershell, need to format html properly

--- PowerShell/O365LicenceReporter.ps1
+++ PowerShell/O365LicenceReporter.ps1
@@ -1,64 +1,94 @@
 $Path = "C:\O365Reports\"
-$Header = @"
-<style>
-table {
-font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-border-collapse: collapse;
-width: 100%;
-}
-th {
-padding-top: 12px;
-padding-bottom: 12px;
-text-align: left;
-background-color: #4CAF50;
-color: white;
-}
-</style>
-"@
 
-try{
-    Import-Module MSOnline
-} catch {
+$TableHeader = "<table class=`"table table-bordered table-hover`" style=`"width:80%`">"
+$Whitespace = "<br/>"
+$TableStyling = "<th>", "<th style=`"background-color:#0d81bb`">"
+$prodlist = Import-Csv .\ProductReference.csv
+$colourlist = @("DarkYellow", "DarkRed", "DarkGreen", "DarkCyan")
+
+try {
+    Import-Module MSOnline 
+}
+catch {
     Install-Module MSOnline
 }
 Connect-MsolService
 
 $PartConts = Get-MsolPartnerContract 
+$coloi = 0
 
-Foreach ($Partner in $PartConts){
+Foreach ($Partner in $PartConts) {
+    
+    # Check if color is on last item ? Reset : Ignore
+    If ($coloi -eq 4) {
+        $coloi = 0
+    }
+
+    # Set Colour for Org
+    $OrgCol = $colourlist[$coloi]
+
     $EndHTML = ""
     $tid = $Partner.tenantid
     $partname = $Partner.Name
     $filepath = $Path + $partname + "_License_Report.html"
-    
+
     $LiceSubs = Get-MsolAccountSku -TenantId $tid
-    $prodlist = Import-Csv .\ProductReference.csv
     $NiceTotal = @()
+    Write-Host "Gathering licence information for" -NoNewline
+    Write-Host "$partname" -ForegroundColor $OrgCol
     
-    Foreach ($licesub in $LiceSubs){
+    $coltoggle = $false
+
+    Foreach ($licesub in $LiceSubs) {
+
+        If ($coltoggle -eq $false) {
+            $licecol = "DarkMagenta"
+        }
+        else {
+            $licecol = "DarkGray"
+        }
+
         $prodref = $licesub.SkuId
-        $ProdNiceName = $prodlist | ? {$prodref -contains $_.GUID}
-        $LiceUsers = Get-Msoluser -tenantid $tid | Where-Object {($_.licenses).AccountSkuID -match ($ProdNiceName).'String ID'} | Select-Object DisplayName, UserPrincipalName
-        If ($ProdNiceName.'Product Name'){
-        $CleanSub = [pscustomobject][ordered]@{
-            Product = $ProdNiceName.'Product name'
-            UnitsTotal = $licesub.ActiveUnits
-            UnitsConsumed = $licesub.ConsumedUnits
-            Users = $LiceUsers
+        $ProdNiceName = $prodlist | ? { $prodref -contains $_.GUID }
+
+        If ($ProdNiceName) {
+            Write-Host "$($partname): " -ForegroundColor $OrgCol -NoNewline
+            Write-Host "$($ProdNiceName.'Product name'): " -ForegroundColor $licecol -NoNewline
+            Write-Host "Found Licence" -ForegroundColor Green
+            $LiceUsers = Get-Msoluser -tenantid $tid | Where-Object { ($_.licenses).AccountSkuID -match ($ProdNiceName).'String ID' } | Select-Object DisplayName, UserPrincipalName
+            Write-Host "$($partname): " -ForegroundColor $OrgCol -NoNewline
+            Write-Host "$($ProdNiceName.'Product name'): " -ForegroundColor $licecol -NoNewline
+            Write-Host "Found $(($LiceUsers | Measure-Object).Count) Users" -ForegroundColor Yellow
+            $AvaiLice = $licesub.ActiveUnits - $licesub.ConsumedUnits
+            If ($AvaiLice -gt 0) {
+                Write-Host "$($partname): " -ForegroundColor $OrgCol -NoNewline
+                Write-Host "$($ProdNiceName.'Product name'): " -ForegroundColor $licecol -NoNewline
+                Write-Host "Found $AvaiLice Available/Unused Licences" -ForegroundColor Yellow
             }
-        $NiceTotal += $CleanSub
+            If ($ProdNiceName.'Product Name') {
+                $CleanSub = [pscustomobject][ordered]@{
+                    Product       = $ProdNiceName.'Product name'
+                    UnitsTotal    = $licesub.ActiveUnits
+                    UnitsConsumed = $licesub.ConsumedUnits
+                    Users         = $LiceUsers
+                }
+                $NiceTotal += $CleanSub
+            }
+            $coltoggle = -not $coltoggle
         }
     }
 
-    $EndHTML += $NiceTotal| Select-Object Product, UnitsTotal, UnitsConsumed | convertto-html -PreContent "<h1> Total Count </h1>" -Head $Header
-    
+    $coloi += 1
 
-    Foreach ($product in $Nicetotal){
+   <#  $TotalHTMLRAW = $NiceTotal | Select-Object Product, UnitsTotal, UnitsConsumed | convertto-html -Fragment | Select-Object -Skip 1
+    $EndHTML += $TableHeader + ($TotalHTMLRAW -replace $TableStyling) + $Whitespace
+
+    Foreach ($product in $Nicetotal) {
         $EndHTML += $Product.Users | ConvertTo-Html -PreContent "<h2>$($product.product) - Users:</h2>" -Head $Header
 
     }
 
-    $endHTML| Out-File $filepath
+    $endHTML | Out-File $filepath #>
 
 }
 
